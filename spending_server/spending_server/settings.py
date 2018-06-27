626e64447ce31bf4df450c9c45ef10896d7e3db1
duhoslav <duhoslav@gmail.com>
2018-06-27 23:52:46 +0300

    + re-structure

--- spending_server/spending_server/settings.py
+++ spending_server/spending_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xeh#7x$gqi^a9=k8gxfw+v62-ctr%=^iao!230=z=mb+a_+!le'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
